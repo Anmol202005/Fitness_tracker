401eec771a38baefd0981ff637331e02d3dd35e1
Anmol <anmol20102005@gmail.com>
2025-01-03 18:21:24 +0530

    added global Exception handler

---
+++ src/main/java/org/fitness/fitness/ExceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,56 @@
+package org.fitness.fitness.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.PatternSyntaxException;
+
+import org.fitness.fitness.Model.DTO.ResponseMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(PatternSyntaxException.class)
+    public ResponseEntity<ResponseMessage> handlePatternSyntaxException(PatternSyntaxException ex) {
+        String errorMessage = "Invalid regex pattern: " + ex.getDescription();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseMessage.builder()
+                .message(errorMessage)
+                .build());
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseMessage> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseMessage.builder()
+                .message(errors.toString())
+                .build());
+    }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ResponseMessage> handleRuntimeException(RuntimeException ex) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ResponseMessage.builder()
+                                                                               .message(ex.getMessage())
+                                                                               .build());
+    }
+
+
+    // Catch-all handler for any other exceptions (optional)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseMessage> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ResponseMessage.builder()
+                                                                                           .message(ex.getMessage())
+                                                                                           .build());
+    }
+
+}
